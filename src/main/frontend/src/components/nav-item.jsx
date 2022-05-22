d4f19beec55065f7f4bf3a8135b628b81b207f1b
ktxdev <sean.ktxdev@gmail.com>
2022-05-22 21:30:13 +0200

    Added other pages

--- src/main/frontend/src/components/nav-item.jsx
+++ src/main/frontend/src/components/nav-item.jsx
@@ -1,58 +1,26 @@
 import React from 'react'
-import { Box, Button, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { Box, Button, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme } from '@mui/material'
 import { Link, NavLink } from 'react-router-dom'
+import { bgcolor } from '@mui/system'
+import { grey } from '@mui/material/colors'
 
 const NavItem = ({ href, icon, title, open, ...others }) => {
     return (
-        <NavLink to={href}>
-            <ListItemButton>
-                <ListItemIcon>
+        <NavLink to={href}
+            style={({isActive}) => ({
+                textDecoration: 'none',
+                color: isActive ? '#1976d2' : 'black'
+            })}
+        >
+            <ListItemButton
+                disableRipple
+            >
+                <ListItemIcon style={{color: 'inherit'}}>
                     {icon}
                 </ListItemIcon>
                 <ListItemText primary={title} />
             </ListItemButton>
         </NavLink>
-        // <ListItem
-        //     disableGutters
-        //     sx={{
-        //         display: 'flex',
-        //         mb: 0.5,
-        //         py: 0,
-        //         px: 2
-        //     }}
-        //     {...others}
-        // >
-        //     <NavLink
-        //         to={href}
-        //         style={({ isActive }) => ({
-        //             backgroundColor: isActive && 'rgba(255,255,255, 0.08)',
-        //             borderRadius: 1,
-        //             color: isActive ? 'secondary.main' : 'neutral.300',
-        //             fontWeight: isActive && 'fontWeightBold',
-        //             justifyContent: 'flex-start',
-        //             px: 3,
-        //             textAlign: 'left',
-        //             textTransform: 'none',
-        //             width: '100%',
-        //             '& .MuiButton-startIcon': {
-        //                 color: isActive ? 'secondary.main' : 'neutral.400'
-        //             },
-        //             '&:hover': {
-        //                 backgroundColor: 'rgba(255,255,255, 0.08)'
-        //             }
-        //         })}
-        //     >
-        //         <Button
-        //             component="a"
-        //             startIcon={icon}
-        //             disableRipple
-        //         >
-        //             {open && <Box sx={{ flexGrow: 1 }}>
-        //                 {title}
-        //             </Box>}
-        //         </Button>
-        //     </NavLink>
-        // </ListItem>
     )
 }
 
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -5,8 +5,13 @@ import App from './App';
 import { AuthProvider, RequireAuth } from './auth/auth';
 import Layout from './components/layout';
 import './index.css';
+import Account from './pages/account';
+import Dashboard from './pages/dashboard';
+import Projects from './pages/projects';
 import SignIn from './pages/sign-in';
 import SignUp from './pages/sign-up';
+import Tickets from './pages/tickets';
+import Users from './pages/users';
 import AlertProvider from './utils/AlertContext';
 
 ReactDOM.render(
@@ -15,7 +20,13 @@ ReactDOM.render(
       <AuthProvider>
         <Routes>
           <Route path='/' element={<App />} >
-            <Route index element={<RequireAuth><Layout /></RequireAuth>} />
+            <Route path='/' element={<RequireAuth><Layout /></RequireAuth>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path='/tickets' element={<Tickets />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/users' element={<Users />} />
+              <Route path='/account' element={<Account />} />
+            </Route>
             <Route path='/sign-in' element={<SignIn />} />
             <Route path='/sign-up' element={<SignUp />} />
           </Route>
