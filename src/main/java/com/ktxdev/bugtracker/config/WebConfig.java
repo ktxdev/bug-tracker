8ca57e470736017456d426c75ffc85933f97bd91
ktxdev <sean.ktxdev@gmail.com>
2022-06-04 14:11:51 +0200

    Added CORS methods in web config

--- src/main/java/com/ktxdev/bugtracker/config/WebConfig.java
+++ src/main/java/com/ktxdev/bugtracker/config/WebConfig.java
@@ -5,11 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static org.springframework.http.HttpMethod.*;
+
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(GET.name(), POST.name(), PUT.name(), DELETE.name());
     }
 }
