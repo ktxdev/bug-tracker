d5b6d15b17a49ade798dffa7aae1c9da98be01c2
ktxdev <sean.ktxdev@gmail.com>
2022-06-18 23:38:08 +0200

    Use java version 11 for heroku

--- src/main/frontend/src/api/api-core.js
+++ src/main/frontend/src/api/api-core.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://api.bug-tracker.ktxdev.com:9803/api/',
+    baseURL: 'http://localhost:8080/api/',
     headers: {
         'content-type': 'application/json'
     }
